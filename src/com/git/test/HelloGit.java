b75158f61c6aaf2b8348d1ede6b72643a6c8c837
zhanglinchun <2601165134@qq.com>
2019-02-19 22:48:13 +0800

    提交

--- src/com/git/test/HelloGit.java
+++ src/com/git/test/HelloGit.java
@@ -4,7 +4,5 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("陪你去看流星雨");
         System.out.println("看你个大头鬼");
-        System.out.println("111111111111");
-        System.out.println("111111111111");
     }
 }
