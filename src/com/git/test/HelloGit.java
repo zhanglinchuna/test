07cf37a11433a8aa8ce76bd5351a4e5133471250
zlc12345 <39194099+zlc12345@users.noreply.github.com>
2019-02-22 23:08:07 +0800

    Update HelloGit.java

--- src/com/git/test/HelloGit.java
+++ src/com/git/test/HelloGit.java
@@ -4,5 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("陪你去看流星雨");
         System.out.println("看你个大头鬼");
+        
+        System.out.println("嘤嘤嘤");
     }
 }
