3bcfe7df897698fde175f9526fcd606aed7a62ef
zhanglinchuna <39194099+zhanglinchuna@users.noreply.github.com>
2020-03-31 22:21:57 +0800

    Update queryChannel.sql

--- queryChannel.sql
+++ queryChannel.sql
@@ -13,12 +13,19 @@ begin
 	fetch c_name
 		into temp_name;
 		
-		select e.sal into temp_sal	from emp e where e.ENAME = temp_name;
+		select count(*) into temp_sal from emp e where e.ENAME = '111';
+		if temp_sal>0 then
+			select e.SAL into temp_sal from emp e where e.ENAME = '111';
+		end if; 
 		
+		  IF SQL%FOUND THEN     --使用隐式游标SQL%FOUND属性，判断数据是否修改成功
+				DBMS_OUTPUT.PUT_LINE('成功修改雇员工资！');
+				EXIT when SQL%NOTFOUND;
+			END IF;
+			
 		EXIT when c_name%NOTFOUND; -- loop循环结束条件
 	DBMS_OUTPUT.PUT_LINE(temp_name);
 	DBMS_OUTPUT.PUT_LINE(temp_sal);
-  
 	END LOOP;
 	CLOSE c_name;
 end;
